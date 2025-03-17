30d879a38c20061d949a9fa7dfce15c1a4b88f09
eyobScript <eyobbmulugeta@gmail.com>
2025-03-17 22:02:49 +0300

    a little bit hint condition

--- GUESS-NUMBER-GAME/guess_number_game.py
+++ GUESS-NUMBER-GAME/guess_number_game.py
@@ -4,4 +4,8 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = input('Guess a number between 1 and ')
+        guess = int(input(f'Guess a number between 1 and {x}: ' ))
+        if guess < random_number:
+            print('Sorry, guess again. Tool low.')
+        elif guess > random_number:
+            print('Sorry, guess again. Too high.')
