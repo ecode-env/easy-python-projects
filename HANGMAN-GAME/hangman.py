aaa5be25fb6619c3d02ae258ece6e1e657f81f20
eyobScript <eyobbmulugeta@gmail.com>
2025-03-18 15:36:29 +0300

    create the check valid word function

--- HANGMAN-GAME/hangman.py
+++ HANGMAN-GAME/hangman.py
@@ -1,3 +1,10 @@
 import random
 from words import words
 
+def get_valid_word(words):
+    word = random.choice(words)
+    if '_' or ' ' in word:
+        word = random.choice(words)
+    return word
+
+print(get_valid_word(words))
